4dbffa497bee39d8e7ac4e9ba91431d036227212
andy840119 <andy840119@gmail.com>
2020-02-01 10:53:39 +0900

    Not sure is this useful. Not need in ruleset now.

---
+++ osu.Framework.Microphone/Input/Bindings/IMicrophoneAction.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+namespace osu.Framework.Input.Bindings
+{
+    public interface IMicrophoneAction
+    {
+        /// <summary>
+        /// Detected pitch
+        /// </summary>
+        public double Pitch { get; set; }
+
+        /// <summary>
+        /// Detected volumn
+        /// </summary>
+        public float Volumn { get; set; }
+    }
+}
