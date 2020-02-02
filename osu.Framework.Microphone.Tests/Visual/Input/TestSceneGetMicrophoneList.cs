f76edb2fac54de0a8c697363780835c6892f8940
andy840119 <andy840119@gmail.com>
2020-02-02 14:34:18 +0900

    Add initial file and test case

---
+++ osu.Framework.Microphone.Tests/Visual/Input/TestSceneGetMicrophoneList.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using osu.Framework.Graphics.UserInterface;
+using osu.Framework.Input;
+
+namespace osu.Framework.Tests.Visual.Input
+{
+    public class TestSceneGetMicrophoneList : FrameworkTestScene
+    {
+        public TestSceneGetMicrophoneList()
+        {
+            var manager = new MicrophoneManager();
+            Child = new BasicDropdown<string>
+            {
+                X = 100,
+                Y = 100,
+                Width = 300,
+                Items = manager.AudioDeviceNames
+            };
+        }
+    }
+}
---
+++ osu.Framework.Microphone/Input/MicrophoneManager.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) ppy Pty Ltd <contact@ppy.sh>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using ManagedBass;
+using osu.Framework.Extensions.TypeExtensions;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace osu.Framework.Input
+{
+    public class MicrophoneManager
+    {
+        private List<DeviceInfo> audioDevices = new List<DeviceInfo>();
+        private List<string> audioDeviceNames = new List<string>();
+
+        /// <summary>
+        /// The names of all available audio devices.
+        /// </summary>
+        /// <remarks>
+        /// This property does not contain the names of disabled audio devices.
+        /// </remarks>
+        public IEnumerable<string> AudioDeviceNames => audioDeviceNames;
+
+        public MicrophoneManager()
+        {
+            // Get device on ctor
+            audioDevices = EnumerateAllDevices().ToList();
+            audioDeviceNames = audioDevices.Skip(1).Where(d => d.IsEnabled).Select(d => d.Name).ToList();
+        }
+
+        protected virtual IEnumerable<DeviceInfo> EnumerateAllDevices()
+        {
+            int deviceCount = Bass.RecordingDeviceCount;
+            for (int i = 0; i < deviceCount; i++)
+                yield return Bass.RecordGetDeviceInfo(i);
+        }
+
+        protected virtual bool IsCurrentDeviceValid()
+        {
+            var deviceIndex = Bass.CurrentRecordingDevice;
+            var device = deviceIndex == Bass.DefaultDevice ? default : Bass.RecordGetDeviceInfo(deviceIndex);
+
+            return device.IsEnabled && device.IsInitialized;
+        }
+
+        public override string ToString() => $@"{GetType().ReadableName()}";
+    }
+}
