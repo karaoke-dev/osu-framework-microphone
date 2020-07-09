6ae97394b0b0b6692d86f1324408e0b6cd7f7900
andy840119 <andy840119@gmail.com>
2020-07-09 22:16:31 +0900

    Update some code.

--- osu.Framework.Microphone/Input/States/MicrophoneInputState.cs
+++ osu.Framework.Microphone/Input/States/MicrophoneInputState.cs
@@ -5,11 +5,27 @@ namespace osu.Framework.Input.States
 {
     public class MicrophoneInputState : InputState, IMicrophoneInputState
     {
+        /// <summary>
+        /// The microphone state.
+        /// </summary>
         public MicrophoneState Microphone { get; }
 
+        /// <summary>
+        /// Creates a new <see cref="InputState"/> using the individual input states from another <see cref="InputState"/>.
+        /// </summary>
+        /// <param name="other">The <see cref="InputState"/> to take the individual input states from. Note that states are not cloned and will remain as references to the same objects.</param>
+        public MicrophoneInputState(MicrophoneInputState other)
+            : this(other.Microphone)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="InputState"/> using given individual input states.
+        /// </summary>
+        /// <param name="microphone">The microphone state.</param>
         public MicrophoneInputState(MicrophoneState microphone)
         {
-            Microphone = microphone;
+            Microphone = microphone ?? new MicrophoneState();
         }
     }
 }
