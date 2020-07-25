a0a422f463988605aec95b98103c5e0ac5bee963
andy840119 <andy840119@gmail.com>
2020-07-25 11:40:09 +0900

    Add ios microphone handler to ask permission.

---
+++ osu.Framework.Microphone.iOS/Input/IOSMicrophoneHandler.cs
@@ -0,0 +1,59 @@
+﻿using AVFoundation;
+using Foundation;
+using osu.Framework.Input.Handlers.Microphone;
+using osu.Framework.Logging;
+using osu.Framework.Platform;
+
+namespace osu.Framework.iOS.Input
+{
+    public class IOSMicrophoneHandler : OsuTKMicrophoneHandler
+    {
+        public override bool IsActive => throw new System.NotImplementedException();
+
+        public IOSMicrophoneHandler(int device) : base(device)
+        {
+        }
+
+        public override bool Initialize(GameHost host)
+        {
+            var session = AVAudioSession.SharedInstance();
+            var success = false;
+
+            Logger.Log("Begin Recording", LoggingTarget.Information, LogLevel.Verbose);
+
+            session.RequestRecordPermission((granted) =>
+            {
+                Logger.Log($"Audio Permission: {granted}", LoggingTarget.Information, LogLevel.Verbose);
+
+                if (granted)
+                {
+                    session.SetCategory(AVAudioSession.CategoryRecord, out NSError error);
+                    if (error == null)
+                    {
+                        session.SetActive(true, out error);
+                        if (error != null)
+                        {
+                            Logger.Log(error.LocalizedDescription, LoggingTarget.Information, LogLevel.Error);
+                        }
+                        else
+                        {
+                            success = base.Initialize(host);
+                            Logger.Log($"Microphone get permission status : {success}", LoggingTarget.Information, LogLevel.Verbose);
+                        }
+                    }
+                    else
+                    {
+                        Logger.Log(error.LocalizedDescription, LoggingTarget.Information, LogLevel.Error);
+                    }
+                }
+                else
+                {
+                    Logger.Log("YOU MUST ENABLE MICROPHONE PERMISSION", LoggingTarget.Information, LogLevel.Error);
+                }
+            });
+
+            Logger.Log($"Checking : {success}", LoggingTarget.Information, LogLevel.Error);
+            return success;
+        }
+    }
+}
