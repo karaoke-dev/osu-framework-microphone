7fc12e5e772e95fd56aada22c074be82280b0a58
andy840119 <andy840119@gmail.com>
2021-09-03 23:08:19 +0900

    create voice class to record pitch and loudness. also use precise instead of compare two float.
    just follow how touch.cs do in osu-framework

---
+++ osu.Framework.Microphone/Input/Voice.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
+// See the LICENCE file in the repository root for full licence text.
+
+using System;
+using osu.Framework.Utils;
+
+namespace osu.Framework.Input
+{
+    public readonly struct Voice : IEquatable<Voice>
+    {
+        /// <summary>
+        /// Detected pitch
+        /// </summary>
+        public readonly float Pitch;
+
+        /// <summary>
+        /// Detected loudness
+        /// </summary>
+        public readonly float Loudness;
+
+        /// <summary>
+        /// Detected voice or not
+        /// </summary>
+        public bool HasVoice => Pitch != 0;
+
+        public Voice(float pitch, float loudness)
+        {
+            Pitch = pitch;
+            Loudness = loudness;
+        }
+
+        /// <summary>
+        /// Indicates whether the two pitch result are the same.
+        /// </summary>
+        /// <param name="other">The other touch.</param>
+        public bool Equals(Voice other) => Precision.AlmostEquals(Pitch, other.Pitch) && Precision.AlmostEquals(Loudness, other.Loudness);
+
+        public static bool operator ==(Voice left, Voice right) => left.Equals(right);
+        public static bool operator !=(Voice left, Voice right) => !(left == right);
+
+        public override bool Equals(object obj) => obj is Voice other && Equals(other);
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Pitch, Loudness);
+        }
+    }
+}
