d7bbbdce879882b783e8c7b9746b2cb2d28abf6c
andy840119 <andy840119@gmail.com>
2024-02-20 00:00:49 +0800

    Fix the api breaking changed made in the https://github.com/ppy/osu-framework/pull/5698/files

--- osu.Framework.Microphone.Tests.iOS/Application.cs
+++ osu.Framework.Microphone.Tests.iOS/Application.cs
@@ -1,14 +1,12 @@
 ﻿// Copyright (c) karaoke.dev <contact@karaoke.dev>. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-using UIKit;
+using osu.Framework.iOS;
 
 namespace osu.Framework.Tests;
 
 public static class Application
 {
-    public static void Main(string[] args)
-    {
-        UIApplication.Main(args, null, typeof(AppDelegate));
-    }
+    // This is the main entry point of the application.
+    public static void Main(string[] args) => GameApplication.Main(new VisualTestGame());
 }
